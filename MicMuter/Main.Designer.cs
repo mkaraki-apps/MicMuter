2af903009efe9fc50a76c6d6db1b895fc4e7caa0
mkaraki <backtrack1994+github@gmail.com>
2019-10-05 08:58:08 +0900

    Add project files.

---
+++ MicMuter/Main.Designer.cs
@@ -0,0 +1,170 @@
+﻿namespace MicMuter
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_startmuter = new System.Windows.Forms.Button();
+            this.btn_stopmuter = new System.Windows.Forms.Button();
+            this.lbl_muterenb = new System.Windows.Forms.Label();
+            this.lmt_mic = new System.Windows.Forms.ProgressBar();
+            this.sbar_micminlvl = new System.Windows.Forms.TrackBar();
+            this.btn_updatedev = new System.Windows.Forms.Button();
+            this.cbox_mic = new System.Windows.Forms.ComboBox();
+            this.cbox_out = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.sbar_micminlvl)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_startmuter
+            // 
+            this.btn_startmuter.Location = new System.Drawing.Point(312, 355);
+            this.btn_startmuter.Name = "btn_startmuter";
+            this.btn_startmuter.Size = new System.Drawing.Size(75, 23);
+            this.btn_startmuter.TabIndex = 0;
+            this.btn_startmuter.Text = "Start";
+            this.btn_startmuter.UseVisualStyleBackColor = true;
+            this.btn_startmuter.Click += new System.EventHandler(this.Btn_startmuter_Click);
+            // 
+            // btn_stopmuter
+            // 
+            this.btn_stopmuter.Location = new System.Drawing.Point(231, 355);
+            this.btn_stopmuter.Name = "btn_stopmuter";
+            this.btn_stopmuter.Size = new System.Drawing.Size(75, 23);
+            this.btn_stopmuter.TabIndex = 1;
+            this.btn_stopmuter.Text = "Stop";
+            this.btn_stopmuter.UseVisualStyleBackColor = true;
+            this.btn_stopmuter.Click += new System.EventHandler(this.Btn_stopmuter_Click);
+            // 
+            // lbl_muterenb
+            // 
+            this.lbl_muterenb.AutoSize = true;
+            this.lbl_muterenb.Location = new System.Drawing.Point(12, 360);
+            this.lbl_muterenb.Name = "lbl_muterenb";
+            this.lbl_muterenb.Size = new System.Drawing.Size(77, 13);
+            this.lbl_muterenb.TabIndex = 2;
+            this.lbl_muterenb.Text = "Muter Stopped";
+            // 
+            // lmt_mic
+            // 
+            this.lmt_mic.Location = new System.Drawing.Point(57, 39);
+            this.lmt_mic.Name = "lmt_mic";
+            this.lmt_mic.Size = new System.Drawing.Size(330, 23);
+            this.lmt_mic.TabIndex = 6;
+            // 
+            // sbar_micminlvl
+            // 
+            this.sbar_micminlvl.Location = new System.Drawing.Point(57, 68);
+            this.sbar_micminlvl.Maximum = 100;
+            this.sbar_micminlvl.Name = "sbar_micminlvl";
+            this.sbar_micminlvl.Size = new System.Drawing.Size(330, 45);
+            this.sbar_micminlvl.TabIndex = 7;
+            // 
+            // btn_updatedev
+            // 
+            this.btn_updatedev.Location = new System.Drawing.Point(150, 355);
+            this.btn_updatedev.Name = "btn_updatedev";
+            this.btn_updatedev.Size = new System.Drawing.Size(75, 23);
+            this.btn_updatedev.TabIndex = 8;
+            this.btn_updatedev.Text = "Update";
+            this.btn_updatedev.UseVisualStyleBackColor = true;
+            this.btn_updatedev.Click += new System.EventHandler(this.Btn_updatedev_Click);
+            // 
+            // cbox_mic
+            // 
+            this.cbox_mic.FormattingEnabled = true;
+            this.cbox_mic.Location = new System.Drawing.Point(57, 12);
+            this.cbox_mic.Name = "cbox_mic";
+            this.cbox_mic.Size = new System.Drawing.Size(330, 21);
+            this.cbox_mic.TabIndex = 9;
+            // 
+            // cbox_out
+            // 
+            this.cbox_out.FormattingEnabled = true;
+            this.cbox_out.Location = new System.Drawing.Point(57, 119);
+            this.cbox_out.Name = "cbox_out";
+            this.cbox_out.Size = new System.Drawing.Size(330, 21);
+            this.cbox_out.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(24, 13);
+            this.label1.TabIndex = 11;
+            this.label1.Text = "Mic";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 12;
+            this.label2.Text = "Output";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(399, 390);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbox_out);
+            this.Controls.Add(this.cbox_mic);
+            this.Controls.Add(this.btn_updatedev);
+            this.Controls.Add(this.sbar_micminlvl);
+            this.Controls.Add(this.lmt_mic);
+            this.Controls.Add(this.lbl_muterenb);
+            this.Controls.Add(this.btn_stopmuter);
+            this.Controls.Add(this.btn_startmuter);
+            this.Name = "Main";
+            this.Text = "MicMuter";
+            this.Load += new System.EventHandler(this.Main_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.sbar_micminlvl)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_startmuter;
+        private System.Windows.Forms.Button btn_stopmuter;
+        private System.Windows.Forms.Label lbl_muterenb;
+        private System.Windows.Forms.ProgressBar lmt_mic;
+        private System.Windows.Forms.TrackBar sbar_micminlvl;
+        private System.Windows.Forms.Button btn_updatedev;
+        private System.Windows.Forms.ComboBox cbox_mic;
+        private System.Windows.Forms.ComboBox cbox_out;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ MicMuter/Main.cs
@@ -0,0 +1,153 @@
+﻿using NAudio.CoreAudioApi;
+using NAudio.Wave;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using NAudio.Dsp;
+
+namespace MicMuter
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        BufferedWaveProvider BufferedWave;
+
+        WaveIn WaveIn = null;
+        WaveOut WaveOut;
+
+        BiQuadFilter[,] Filters;
+
+        private void Btn_startmuter_Click(object sender, EventArgs e)
+        {
+            if (WaveIn != null)
+            {
+                MessageBox.Show("Muter Already started.");
+                return;
+            }
+
+            if (cbox_mic.SelectedItem == null)
+            {
+                MessageBox.Show("Mic not selected");
+                return;
+            }
+            else if (cbox_out.SelectedItem == null)
+            {
+                MessageBox.Show("Output not selected");
+                return;
+            }
+
+            WaveIn = new WaveIn();
+            WaveIn.DeviceNumber = cbox_mic.SelectedIndex;
+            WaveOut = new WaveOut();
+            WaveOut.DeviceNumber = cbox_out.SelectedIndex;
+
+            BufferedWave = new BufferedWaveProvider(WaveIn.WaveFormat);
+            BufferedWave.DiscardOnBufferOverflow = true;
+
+            CreateFilters(BufferedWave.WaveFormat.Channels, BufferedWave.WaveFormat.SampleRate, Eq.NonVoiceCutEq);
+
+            WaveIn.DataAvailable += WaveIn_DataAvailable;
+
+            WaveOut.Init(BufferedWave);
+            WaveIn.StartRecording();
+            WaveOut.Play();
+
+            lbl_muterenb.Text = "Muter Started";
+        }
+
+        private void CreateFilters(int channel, float sampleRate, EqBand[] eqs)
+        {
+            Filters = new BiQuadFilter[channel, eqs.Length];
+
+            for (int b = 0; b < eqs.Length; b++)
+            {
+                var eqi = eqs[b];
+                for (int c = 0; c < channel; c++)
+                {
+                    Filters[c, b] = BiQuadFilter.PeakingEQ(sampleRate, eqi.Frequency, eqi.Bandwidth, eqi.Gain);
+                }
+            }
+        }
+
+        private void WaveIn_DataAvailable(object sender, WaveInEventArgs e)
+        {
+            // Original Code: https://github.com/naudio/NAudio/blob/792d3cc0ea51cac8d5b8510b3b68268a8a697ae8/Docs/RecordingLevelMeter.md
+            float max = 0;
+            // interpret as 16 bit audio
+            for (int index = 0; index < e.BytesRecorded; index += 2)
+            {
+                short sample = (short)((e.Buffer[index + 1] << 8) |
+                                        e.Buffer[index + 0]);
+                // to floating point
+                var sample32 = Math.Abs(sample / 32768f);
+                // is this the max value?
+                if (sample32 > max) max = sample32;
+            }
+
+            int lvl = (int)(max * 100);
+            lmt_mic.Value = (int)(max * 100);
+
+            if (lvl > sbar_micminlvl.Value)
+            {
+                BufferedWave.AddSamples(e.Buffer, 0, e.BytesRecorded);
+            }
+            else
+            {
+                BufferedWave.AddSamples(new byte[e.BytesRecorded], 0, e.BytesRecorded);
+            }
+        }
+
+        private void Btn_stopmuter_Click(object sender, EventArgs e)
+        {
+            if (WaveIn == null)
+            {
+                MessageBox.Show("Muter not started.");
+                return;
+            }
+
+            WaveIn.StopRecording();
+            WaveIn.Dispose();
+            WaveOut.Stop();
+            WaveOut.Dispose();
+            BufferedWave.ClearBuffer();
+
+            WaveIn = null;
+            lbl_muterenb.Text = "Muter Stopped";
+        }
+
+        private void Main_Load(object sender, EventArgs e)
+        {
+            Btn_updatedev_Click(null, null);
+        }
+
+        private void Btn_updatedev_Click(object sender, EventArgs e)
+        {
+            cbox_mic.Items.Clear();
+            cbox_out.Items.Clear();
+
+            int ids = WaveIn.DeviceCount;
+            for (int id = 0; id < ids; id++)
+            {
+                WaveInCapabilities deviceInfo = WaveIn.GetCapabilities(id);
+                cbox_mic.Items.Add(deviceInfo.ProductName);
+            }
+
+            int ods = WaveOut.DeviceCount;
+            for (int od = 0; od < ods; od++)
+            {
+                WaveOutCapabilities deviceInfo = WaveOut.GetCapabilities(od);
+                cbox_out.Items.Add(deviceInfo.ProductName);
+            }
+        }
+    }
+}
